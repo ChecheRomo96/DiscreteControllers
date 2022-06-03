8dbb28dc974b07e6716f8a0c5f4071ba827ceb85
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2022-06-03 01:17:24 -0500

    -

--- src/Timing/Timing.cpp
+++ src/Timing/Timing.cpp
@@ -4,12 +4,26 @@ uint64_t(*DiscreteControllers::Timing::_ClockUpdate)()
 uint8_t DiscreteControllers::Timing::ClockTimeBase;
 uint64_t DiscreteControllers::Timing::CurrentTime;
 
+void DiscreteControllers::Timing::Start()
+{
+	DiscreteControllers::Timing::_ClockUpdate = NULL;
+	DiscreteControllers::Timing::ClockTimeBase = DiscreteControllers::Timing::TimeBase_Unknown;
+}
+
 void DiscreteControllers::Timing::Start(uint64_t(&ClockUpdate)(), uint8_t TimeBase)
 {
-	
+	_ClockUpdate = 0;
+	ClockTimeBase = TimeBase;
 }
 
 uint64_t DiscreteControllers::Timing::UpdateClock()
 {
-	
+	CurrentTime = 0;
+
+	if(_ClockUpdate != NULL)
+	{
+		CurrentTime = _ClockUpdate();
+	}
+
+	return CurrentTime;
 }
--- src/Timing/Timing.h
+++ src/Timing/Timing.h
@@ -10,11 +10,13 @@
 			static constexpr uint8_t TimeBase_Seconds = 0u;
 			static constexpr uint8_t TimeBase_Milliseconds = 1u;
 			static constexpr uint8_t TimeBase_Microseconds = 2u;
+			static constexpr uint8_t TimeBase_Unknown = 3u;
 
 			extern uint64_t(*_ClockUpdate)();
 			extern uint8_t ClockTimeBase;
 			extern uint64_t CurrentTime;
 
+			void Start();
 			void Start(uint64_t(&ClockUpdate)(), uint8_t TimeBase);
 			uint64_t UpdateClock();
 		}
