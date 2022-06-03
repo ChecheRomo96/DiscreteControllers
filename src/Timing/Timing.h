fe89c8ef0e24c23aef4798f0dec2b7d26acb8dab
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2022-06-03 01:12:57 -0500

    Update Timing.h

--- src/Timing/Timing.h
+++ src/Timing/Timing.h
@@ -11,12 +11,12 @@
 			static constexpr uint8_t TimeBase_Milliseconds = 1u;
 			static constexpr uint8_t TimeBase_Microseconds = 2u;
 
-			extern uint32_t(*_ClockUpdate)();
+			extern uint64_t(*_ClockUpdate)();
 			extern uint8_t ClockTimeBase;
-			extern uint32_t CurrentTime;
+			extern uint64_t CurrentTime;
 
-			void Start(uint32_t(&ClockUpdate)(), uint8_t TimeBase);
-			uint32_t UpdateClock();
+			void Start(uint64_t(&ClockUpdate)(), uint8_t TimeBase);
+			uint64_t UpdateClock();
 		}
 	}
 
