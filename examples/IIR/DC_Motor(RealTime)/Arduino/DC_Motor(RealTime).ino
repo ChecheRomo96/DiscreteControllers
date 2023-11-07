7fdf301a2f29e56377189ea1690b708756e34cdc
José Manuel Romo <79122732+ChecheRomo96@users.noreply.github.com>
2023-11-06 23:46:59 -0600

    Create DC_Motor(RealTime).ino

---
+++ examples/IIR/DC_Motor(RealTime)/Arduino/DC_Motor(RealTime).ino
@@ -0,0 +1,49 @@
+#include <DiscreteControllers.h>
+
+//////////////////////////////////////////////////////////////////////
+// DC Motor transfer function
+// 
+// Note that the motor model expects a signal ranging from -1 to 1, 
+// representing the PWM pulse width and direction of the DC Motor.
+// Also note that it was discretized with a sample frequency of 100 Hz.
+
+  DiscreteControllers::IIR<float, float> Motor({26.700957475450720 }, {1, -0.778800783071405 } );
+  float Fs = 100;
+  float Ts = 1.0 / Fs;
+  float SetPoint = 0, SystemResponse = 0;
+//
+//////////////////////////////////////////////////////////////////////
+// System time variable that do not depends on micros() or millis() 
+ 
+    float time = 0;
+//
+//////////////////////////////////////////////////////////////////////
+// Setup
+
+  void setup() {
+    Serial.begin(115200);
+    
+    Motor.InitializeOutputBuffer();
+    Motor.InitializeInputBuffer();
+  }
+//
+//////////////////////////////////////////////////////////////////////
+// loop
+
+  void loop() {
+    if(Serial.available()){
+      float x = Serial.parseFloat();
+      SetPoint = x;
+      while (Serial.available()) Serial.read();
+    }
+
+    SystemResponse = Motor.Update(SetPoint);
+
+    Serial.print( "SetPoint:" + String(SetPoint * 120.71) );   
+    Serial.println( ",SystemResponse:" + String(SystemResponse) );
+
+    time+=Ts;
+    delay(Ts*1000);
+  }
+//
+//////////////////////////////////////////////////////////////////////
